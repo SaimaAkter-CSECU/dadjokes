af56ca989de59def07f862b636066aac735945e3
SaimaAkter-CSECU <saimaakterzuma@gmail.com>
2022-01-31 20:10:23 +0600

    Commit

---
+++ components/Joke.vue
@@ -0,0 +1,22 @@
+<template>
+    <nuxt-link :to="'jokes/'+id">
+        <div class="joke">
+            <p>{{joke}}</p>
+        </div>
+    </nuxt-link>
+</template>
+
+<script>
+    export default {
+        name: 'Joke',
+        props: ['joke', 'id']
+    }
+</script>
+
+<style scoped>
+    .joke{
+        padding: 1rem; 
+        border-bottom: 1px solid rgba(255,255,255,0.5);
+
+    }
+</style>
---
+++ components/SearchJokes.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <form @submit.prevent="OnSubmit">
+            <input type="text" v-model="text" placeholder="Search Jokes...">
+            <input type="submit" value="Seach jokes"> 
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SearchJokes',
+        data(){
+            return{
+                text: ''
+            }
+        },
+        methods:{
+            OnSubmit(){
+                this.$emit("search-text", this.text); 
+                this.text = ''; 
+            }
+        }
+    }
+</script>
